65ebcac93327d5ee55705add943dfd737eb84656
Teegan Swanson <tcswanny@gmail.com>
2022-04-26 23:07:24 -0500

    Added final aggregate MovieTotalGross to find total gross income of all movies

---
+++ DatabaseProject/Data/Sql/Procedures/Movie.MovieTotalGross.sql
@@ -0,0 +1,19 @@
+﻿CREATE OR ALTER PROCEDURE Movie.MovieTotalGross
+AS
+
+WITH Aggregates(ShowingID, MovieID, GrossSales) AS
+(
+	SELECT S.ShowingID, S.MovieID, COUNT(SA.ShowingID) * S.TicketPrice AS GrossSales
+	FROM Movie.Showing S
+		INNER JOIN Movie.ShowingAttendance SA ON SA.ShowingID = S.ShowingID
+	GROUP BY S.ShowingID, S.MovieID, S.TicketPrice
+)
+
+SELECT M.[Name], M.Genre, M.ReleaseDate, D.[Name], SUM(A.GrossSales) AS GrossSales
+FROM Movie.Movie M
+	INNER JOIN Movie.Director D ON D.DirectorID = M.DirectorID
+	INNER JOIN Aggregates A ON A.MovieID = M.MovieID
+GROUP BY M.[Name], M.Genre, M.ReleaseDate, D.[Name]
+ORDER BY GrossSales DESC
+
+GO
