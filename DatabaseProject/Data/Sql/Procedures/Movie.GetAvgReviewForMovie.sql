0b5adb490c58ca2fa06e70c7cb7bdcbb3c9cc08a
connor39 <connor39@ksu.edu>
2022-04-28 16:52:52 -0500

    GetAvgReview Fixed

--- DatabaseProject/Data/Sql/Procedures/Movie.GetAvgReviewForMovie.sql
+++ DatabaseProject/Data/Sql/Procedures/Movie.GetAvgReviewForMovie.sql
@@ -2,8 +2,13 @@
 	@MovieID INT
 AS
 
-SELECT IIF(COUNT(R.ReviewID) IS NOT NULL, CAST(SUM(R.Rating) AS DECIMAL(16,2))/COUNT(R.ReviewID), 0) AS AvgRatingForMovie
+--SELECT ISNULL(CAST(SUM(R.Rating) AS DECIMAL(16,2)) / COUNT(*),0) AS AvgRatingForMovie
+--FROM Movie.Movie M
+--	INNER JOIN Movie.Review R ON R.MovieID = M.MovieID
+--WHERE M.MovieID = @MovieID
+SELECT IIF(CAST(SUM(R.Rating) AS DECIMAL(16,2))/COUNT(R.ReviewID) IS NOT NULL, CAST(SUM(R.Rating) AS DECIMAL(16,2))/COUNT(R.ReviewID), 0) AS AvgRatingForMovie
 FROM Movie.Movie M
 	INNER JOIN Movie.Review R ON R.MovieID = M.MovieID
 WHERE M.MovieID = @MovieID
+
 GO
