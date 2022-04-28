cca97fb12dd26b7806e1ab5d7fa2301208b052e9
Teegan Swanson <tcswanny@gmail.com>
2022-04-27 22:34:30 -0500

    Finished final aggregate query Movie.RetrieveUserActivity

---
+++ DatabaseProject/Data/Sql/Procedures/Movie.RetrieveUserActivity.sql
@@ -0,0 +1,21 @@
+﻿CREATE OR ALTER PROCEDURE Movie.RetrieveUserActivity
+	@HighestRank INT
+AS
+
+WITH MoviesAttended(UserID, Username, MoviesAttended) AS 
+(
+	SELECT U.UserID, U.Username, COUNT(*) AS MoviesAttended
+	FROM Movie.[User] U
+		INNER JOIN Movie.ShowingAttendance SA ON SA.UserID = U.UserID
+	GROUP BY U.UserID, U.Username
+)
+
+SELECT MA.Username, MA.MoviesAttended, COUNT(*) AS ReviewCount, RANK() OVER (ORDER BY MA.MoviesAttended DESC, COUNT(*) DESC) AS [Rank]
+FROM MoviesAttended MA
+	INNER JOIN Movie.Review R ON R.UserID = MA.UserID
+GROUP BY MA.Username, MA.MoviesAttended
+ORDER BY [Rank] ASC
+OFFSET 0 ROWS
+FETCH NEXT @HighestRank ROWS ONLY
+
+GO
